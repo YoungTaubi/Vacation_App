adecc221b38ba580c8d7e85079ce000fc5363b7d
Jonas Darley <jonasdarley@live.com>
2022-03-02 12:26:19 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+        
+
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
